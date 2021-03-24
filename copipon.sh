f49d3495bb7e18665e0cb3c40ffabf6fe7326621
Fernando Perez <Fernando.Perez@berkeley.edu>
2021-03-24 16:11:39 -0700

    Add note about path configs

--- copipon.sh
+++ copipon.sh
@@ -16,6 +16,11 @@ export PIP_USER=True
 export PREFIX
 export PYTHONUSERBASE=$PREFIX
 export PATH=$PREFIX/bin:$PATH
+# Note - ideally JUPYTER_PATH wouldn't need to be set separately of other Python
+# variables, as e.g. JupyterLab extensions can be pip-installed. But the process
+# of finding their non-python pieces is complex and how to do it without extra
+# info isn't settled, so for now we need an extra explicit variable.
+# See https://github.com/jupyter/jupyter_core/pull/209 for lots of details...
 export JUPYTER_PATH=$PREFIX/share/jupyter:$JUPYTER_PATH
 
 echo "*** Environment developer overlay active at PREFIX=$PREFIX"  # dbg
